3cfb2eef6f21c93759ee9f46c2fb3e54150210f3
mckrava <mckrava2@gmail.com>
2026-01-29 14:54:10 -0500

    feat: enabled all cors

--- apps/hydration-data-lake-aggregator/src/main.ts
+++ apps/hydration-data-lake-aggregator/src/main.ts
@@ -10,6 +10,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for development
+  app.enableCors();
+
   // Enable global validation pipes
   app.useGlobalPipes(
     new ValidationPipe({
